0d53f30ba01e2652a25ec2a3cf6365e143587b77
Hudson Beebe <102378704+Hbeebe@users.noreply.github.com>
2022-09-11 23:01:32 -0700

    Create bunny.java

---
+++ bunny.java
@@ -0,0 +1,10 @@
+public class PrintBunny {
+   public static void main(String[] args) {
+      // write your code here below
+      System.out.println("(" + "\\" + "(" + "\\");
+      System.out.println("(" + "-" + "." + "-)");
+      System.out.println("O_(" + "\"" + ")" + "(" + "\"" + ")");
+      
+      
+   }
+}
